6521f2b62e6b0dbd25d4cde03437a8c74b3a5b8a
Sowayi <sapwao@gmail.com>
2019-06-21 10:16:01 -0400

    Add method save_profile to save a user's profile to the database

--- user_profile/models.py
+++ user_profile/models.py
@@ -8,4 +8,9 @@ class Profile(models.Model):
     bio=models.TextField()
     profile_photo = models.ImageField(upload_to='profile_photos/', blank=True)
     
-    
+    def save_profile(self):
+        """
+        save_image method to check whether an instance of image
+        is successfully stored to the database
+        """
+        self.save()
