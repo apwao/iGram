56c389d0a0e6bb33296218491ebe53c04309a32f
Sowayi <sapwao@gmail.com>
2019-06-23 10:46:10 -0400

    Add url paths for signing up and email confirmation

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
+# from django.contrib.auth import views
+from django.conf import settings 
 
 urlpatterns  = [
-     url('^', views.hello,name='hello')
+     # url('^signup/', views.register, name='register'),
+     url(r'^$', views.signup, name='signup'),
+     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+     
 ]
