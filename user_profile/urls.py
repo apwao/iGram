e2aaa0ffb813a6fff514cc2abe39b61c3d0d4b74
Sowayi <sapwao@gmail.com>
2019-06-23 11:04:56 -0400

    Add url patterns for  creating a profile and viewing a profile

---
+++ user_profile/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns=[
+    url('^$', views.create_profile, name='create_profile'),
+    url('^view_profile$', views.view_profile, name='view_profile')
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
