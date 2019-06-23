be0033c8ad3af7b24c62cc87ae384dc31cd8143e
Sowayi <sapwao@gmail.com>
2019-06-23 10:54:17 -0400

    Add the url paths to be included in the project based on their apps

--- iGram/urls.py
+++ iGram/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 import authenticate
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('authenticate.urls')),
+    
+    # url('^accounts/', include('registration.backends.simple.urls'))
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'',include('images.urls')),
+    url(r'^profile/',include('user_profile.urls')),
+    url(r'^tinymce/',include('tinymce.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^',include('authenticate.urls')),
 ]
