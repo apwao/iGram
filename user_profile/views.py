31c13c62c9e116425253da2528c0ec09532f67de
Sowayi <sapwao@gmail.com>
2019-06-23 11:05:49 -0400

    Add create_profile view function to enable a user to create their profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
+from .models import Profile
 
 # Create your views here.
+def create_profile(request):
+    """
+    """
+    current_user = request.user 
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            created_profile = form.save(commit=False)
+            created_profile.save()
+        return redirect('view_profile')
+    
+    else:
+        form=ProfileForm()
+    return render(request, 'profile_form.html',{'form':form})
+
+
+def view_profile(request):
+    """
+    """
+    return render(request, 'profile.html')
