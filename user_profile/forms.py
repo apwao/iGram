65bc7cd4a4535409e039bc2c293e200d24f719dc
Sowayi <sapwao@gmail.com>
2019-06-24 22:45:07 -0400

    Define the fields to be excluded from the ProfileForm when displayed

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -3,9 +3,9 @@ from .models import Profile
 # from django.contrib.auth.forms import UserCreationForm
 # from django.contrib.auth.models import User
 
-class ProfileForm(forms.Form):
+class ProfileForm(forms.ModelForm):
     """
     """
     class Meta:
         model=Profile
-        fields=('bio','profile_photo')
+        exclude=['user_profile_photo']
