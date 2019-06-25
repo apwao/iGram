3f31fd87244ed47bc49b5b230f109bc10596e4b0
Sowayi <sapwao@gmail.com>
2019-06-24 22:29:40 -0400

    Fix a typing error in the email activation url

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns  = [
      # url('^signup/', views.register, name='register'),
      url(r'^$', views.signup, name='signup'),
-     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+       views.activate, name='activate'),
      
 ]
