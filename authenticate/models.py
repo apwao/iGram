65ba79db40535c62dc73c0aa30427a5d8ad44505
Sowayi <sapwao@gmail.com>
2019-06-24 22:48:59 -0400

    Add user profile photo id column to the profile table

--- authenticate/models.py
+++ authenticate/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+# from django.contrib.auth.models 
 
 # Create your models here.
+# class ExampleModel(models.Model):
+#     model_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
