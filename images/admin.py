462f264f5ab07c972ed6d4b53521c41e5d081fb1
Sowayi <sapwao@gmail.com>
2019-06-26 08:28:13 -0400

    Add admin configurations

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Image,Comment,Followers
+
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Followers)
